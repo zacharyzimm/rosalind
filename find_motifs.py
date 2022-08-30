f31c0c5cd466aa5bfc7480534da79cb94c6246e4
zacharyzimm <105132282+zacharyzimm@users.noreply.github.com>
2022-08-30 16:14:39 -0700

    Update find_motifs.py
    Updated to read from a text file instead

--- find_motifs.py
+++ find_motifs.py
@@ -1,12 +1,18 @@
+import io
+
 #Finding a motif in DNA
 #Given two strings of DNA s and t,
 #Find all locations of t as a substring of s
 
-s = 'GATATATGCATATACTT'
-t = 'ATAT'
-
+f = open('rosalind_subs.txt')
+lines = f.readlines()
+s = lines[0].strip('\n')
+t = lines[1].strip('\n')
 subsections = {i: s[i:i+len(t)] 
                for i in range(0, len(s)) 
                if len(s[i:i+len(t)]) == len(t)}
 
-[k + 1 for k, v in subsections.items() if v == t] #Question calls for 1-indexed strings
+result = [k + 1 for k, v in subsections.items() if v == t]
+
+for item in result:
+    print(item,end=' ')
